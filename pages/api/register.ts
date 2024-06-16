ab46e27f4f1097c469447a5ac058741f38e603ba
wswiselife <wswiselife@gmail.com>
2024-06-17 04:55:09 +0800

    20240616 完毕

---
+++ pages/api/register.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== "POST") {
+        return res.status(405).end();
+    }
+
+    try {
+    } catch (error) {
+        console.log("error", error);
+        return res.status(400).end();
+    }
+}
